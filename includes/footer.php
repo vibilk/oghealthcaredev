6ce2d0a5ca61871b84ffbb7cb543153f175b7a8a
vibilk <113964282+vibilk@users.noreply.github.com>
2022-10-15 12:56:23 +0530

    Updated define, footer and header php file

---
+++ includes/footer.php
@@ -0,0 +1,152 @@
+
+
+ <!-- footer section start  -->
+
+ <footer class="footer_section pb-0">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="footer_logo">
+            <img src="assets/images/og-logo.webp" alt="footer logo">
+          </div>
+        </div>
+        <div class="col">
+          <h4 class="sitemap_title">Careers</h4>
+          <ul class="sitemap_link">
+            <li><a href="">About</a> </li>
+            <li><a href="">Medical Scribing</a> </li>
+            <li><a href="">Open Positions</a> </li>
+            <li><a href="">Career Path Calculator</a> </li>
+            <li><a href="">FAQ</a> </li>
+            <li><a href="">Diversity & Inclusivity</a> </li>
+          </ul>
+          <a href="" class="footer_apply">Apply Here</a>
+        </div>
+        <div class="col">
+          <h4 class="sitemap_title">Tools</h4>
+          <ul class="sitemap_link">
+            <li><a href="">Calculator</a> </li>
+          </ul>
+        </div>
+        <div class="col">
+          <h4 class="sitemap_title">Useful links</h4>
+          <ul class="sitemap_link">
+            <li><a href="">Company</a> </li>
+            <li><a href="">Life</a> </li>
+            <li><a href="">Blog</a> </li>
+          </ul>
+        </div>
+        <div class="col">
+          <h4 class="sitemap_title">Legal</h4>
+          <ul class="sitemap_link">
+            <li><a href="">Privacy Policy</a> </li>
+            <li><a href="">Terms of Use</a> </li>
+            <li><a href="">GDPR</a> </li>
+          </ul>
+        </div>
+      </div>
+      <div class="row py-3">
+        <div class="col-12 text-center">
+          <!-- <p class="m-0 text-white copy_right">© 2022 OG Healthcare</p> -->
+          <p class="m-0 text-white copy_right">&copy;<script>document.write(new Date().getFullYear())</script> OG Healthcare</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+  <!-- footer section end  -->
+
+
+
+  <!-- Cookie modal  -->
+  <div id="cookiebarBox" class="os-animation" data-os-animation="fadeIn">
+    <div class="risk-dismiss" style="display: block;">
+      <div class="d-flex align-items-center justify-content-between px-4 cookie_content">
+        <p class="m-0">We use necessary cookies only. No personalized cookies are processed by OG Healthcare.
+
+        </p>
+        <a id="cookieBoxok" class="cookieok" data-cookie="risk">Got it</a>
+      </div>
+    </div>
+  </div>
+
+  <!-- <div class="scrollup"><i class="fas fa-arrow-up"></i></div> -->
+
+  <div id="progress">
+    <span id="progress-value"><i class="fas fa-arrow-up"></i></span>
+  </div>
+
+
+
+  <!-- fixed faq start  -->
+  <section class="fixed_faq">
+    <div class="help_tag">
+      <p class="m-0"><i class="far fa-question-circle mx-2"></i>Need help?</p>
+    </div>
+    <div class="fixed_faq-container">
+      <div class="fixed_faq-header d-flex align-items-center justify-content-between ">
+        <h2 class="m-0">OG Healthcare Help</h2>
+        <div class="faq_close"><i class="fas fa-times"></i></div>
+      </div>
+      <div class="fixed_faq-body">
+        <div class="support_col">
+          <ul class="nav nav-tabs fixed_faq-tab" role="tablist">
+            <li class="nav-item">
+              <a class="nav-link active" data-bs-toggle="tab" href="#support">Support links</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-bs-toggle="tab" href="#faq">FAQs</a>
+            </li>
+          </ul>
+
+          <div class="tab-content">
+            <div id="support" class="container tab-pane active">
+              <p class="support_link my-4">
+                <a href="tel:">
+                  <span class="d-block">Telephone Numbers</span>
+                  <span class="call_link">Call OG Healthcare Customer Support</span>
+                </a>
+              </p>
+            </div>
+            <div id="faq" class="container tab-pane fade">
+
+              <div class="my-4">
+                <form>
+                  <select class="form-select" id="sel1" name="sellist1">
+                    <option value="" selected disabled>Select category</option>
+                    <option value="">Authentication</option>
+                    <option value="">Fraud</option>
+                    <option value="">General</option>
+                    <option value="">Logon</option>
+                    <option value="">Payments</option>
+                    <option value="">Registration and Navigation</option>
+                    <option value="">Security Device</option>
+                    <option value="">Services</option>
+                    <option value="">User Management</option>
+                  </select>
+                </form>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+
+        
+      </div>
+    </div>
+  </section>
+  <!-- fixed faq end  -->
+
+
+
+  <script src="<?= BASEURL ?>assets/js/jquery.min.js"></script>
+  <script src="<?= BASEURL ?>assets/js/bootstrap.bundle.min.js"></script>
+  <script src="<?= BASEURL ?>assets/js/owl.carousel.min.js"></script>
+  <!-- <script src="assets/js/jquery.mousewheel.min.js"></script> -->
+  <script src="<?= BASEURL ?>assets/js/jquery.canvasjs.min.js"></script>
+  <script src="<?= BASEURL ?>assets/js/aos.js"></script>
+  <script src="<?= BASEURL ?>assets/js/swiper-bundle.min.js"></script>
+  <script src="<?= BASEURL ?>assets/js/custom.js"></script>
+  <script src="<?= BASEURL ?>assets/js/switchmode.js"></script>
+
