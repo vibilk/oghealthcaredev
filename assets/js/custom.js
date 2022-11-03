6c394a2d656ae4259183ed4182a8e8584affa0ab
vibilk <113964282+vibilk@users.noreply.github.com>
2022-11-03 17:06:54 +0530

    Updated js file

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,10 @@
-// $(window).resize(function(){location.reload();});
+// $(window).on('resize',function(){
+//   location.reload();
+// });
+
+
+
+
 
 history.scrollRestoration = "manual";
 $(window).on('beforeunload', function(){
@@ -462,6 +468,28 @@ $(document).mouseup(function(e)
       $(".fixed_faq").removeClass('r-w-0');
     }
 });
+
+ var $post = $(".fixed_faq");
+setInterval(function(){
+    $post.toggleClass("fixed_faq-hide");
+}, 10000);
+
+$(".fixed_faq, .help_tag").hover(function () {
+  $(".fixed_faq").removeClass("fixed_faq-hide");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 $(".dark").on('click', function (e) {
